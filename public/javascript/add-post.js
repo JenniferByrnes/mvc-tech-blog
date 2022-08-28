f9caf573b2593fcb060b99f60db50a1659db99c0
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-08-28 14:08:28 -0400

    working through format changes

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -20,6 +20,17 @@ async function newFormHandler(event) {
   } else {
     alert(response.statusText);
   }
+  document.getElementById('display-posts').style.display='block';
+  document.getElementById('create-post').style.display='none';
+}
+
+function toggleHide(event) {
+  event.preventDefault();
+  console.log("here i am ************************")
+  document.getElementById('display-posts').style.display='none';
+  document.getElementById('create-post').style.display='block';
 }
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+
+document.querySelector('.new-post-form').addEventListener('toggle', toggleHide);
